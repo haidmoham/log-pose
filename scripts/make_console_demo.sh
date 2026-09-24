c4b120ef28ca278a637cb917c484f0abf59a7d94
Mohammad Haider <haidmoham@gmail.com>
2026-09-24 13:01:59 -0400

    fix(demo): encode standard limited-range video

--- scripts/make_console_demo.sh
+++ scripts/make_console_demo.sh
@@ -23,18 +23,18 @@ ffmpeg -hide_banner -loglevel error -y \
   -loop 1 -t 2.8 -i "${frames[2]}" \
   -loop 1 -t 2.8 -i "${frames[3]}" \
   -filter_complex "
-    [0:v]scale=1920:896:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:color=0x211922,setsar=1,fps=30,trim=duration=2.8,setpts=PTS-STARTPTS[v0];
-    [1:v]scale=1920:896:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:color=0x211922,setsar=1,fps=30,trim=duration=2.8,setpts=PTS-STARTPTS[v1];
-    [2:v]scale=1920:896:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:color=0x211922,setsar=1,fps=30,trim=duration=2.8,setpts=PTS-STARTPTS[v2];
-    [3:v]scale=1920:896:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:color=0x211922,setsar=1,fps=30,trim=duration=2.8,setpts=PTS-STARTPTS[v3];
+    [0:v]scale=1920:896:force_original_aspect_ratio=decrease:out_range=tv,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:color=0x211922,setsar=1,fps=30,trim=duration=2.8,setpts=PTS-STARTPTS[v0];
+    [1:v]scale=1920:896:force_original_aspect_ratio=decrease:out_range=tv,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:color=0x211922,setsar=1,fps=30,trim=duration=2.8,setpts=PTS-STARTPTS[v1];
+    [2:v]scale=1920:896:force_original_aspect_ratio=decrease:out_range=tv,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:color=0x211922,setsar=1,fps=30,trim=duration=2.8,setpts=PTS-STARTPTS[v2];
+    [3:v]scale=1920:896:force_original_aspect_ratio=decrease:out_range=tv,pad=1920:1080:(ow-iw)/2:(oh-ih)/2:color=0x211922,setsar=1,fps=30,trim=duration=2.8,setpts=PTS-STARTPTS[v3];
     [v0][v1]xfade=transition=fade:duration=0.4:offset=2.4[x1];
     [x1][v2]xfade=transition=fade:duration=0.4:offset=4.8[x2];
-    [x2][v3]xfade=transition=fade:duration=0.4:offset=7.2[outv]
+    [x2][v3]xfade=transition=fade:duration=0.4:offset=7.2,format=yuv420p,setparams=range=limited[outv]
   " \
   -map "[outv]" -an -c:v libx264 -preset medium -crf 18 \
-  -pix_fmt yuv420p -movflags +faststart -t 10 "$output"
+  -pix_fmt yuv420p -color_range tv -movflags +faststart -t 10 "$output"
 
 ffprobe -v error -show_entries format=duration \
-  -show_entries stream=codec_name,width,height,pix_fmt,r_frame_rate \
+  -show_entries stream=codec_name,width,height,pix_fmt,color_range,r_frame_rate \
   -of default=noprint_wrappers=1 "$output"
 printf '%s\n' "$output"
